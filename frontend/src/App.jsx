65cc86c96e8f9bf93e3bffe684dbb11aef783875
Shivang <negi1322003@gmail.com>
2024-09-03 14:53:21 +0530

    removed App.css from App.jsx frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
